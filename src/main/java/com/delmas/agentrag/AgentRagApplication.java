0a7c81837aa67cb9313e1dff8cf9c6737139b722
delmas007 <azertyazerty1ze@gmail.com>
2025-02-11 09:32:27 +0000

    init

---
+++ src/main/java/com/delmas/agentrag/AgentRagApplication.java
@@ -0,0 +1,13 @@
+package com.delmas.agentrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentRagApplication.class, args);
+    }
+
+}
